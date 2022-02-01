5d3ab4ad0641253cfc60d043a1a1859cd8b2a3d2
David McEwen Arango <dmcewena@hotmail.com>
2022-02-01 16:59:57 -0500

    environment done

---
+++ handyman/handyman-be/handymanbe/src/main/java/com/handyman/handymanbe/configuration/DatabaseConfiguration.java
@@ -0,0 +1,29 @@
+package com.handyman.handymanbe.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/handymanbd");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(
+            DataSource dataSource
+    ) {
+        return new JdbcTemplate(dataSource);
+    }
+}
